796b5ad925d75097fbf0bb5ea0662320a229c9ec
emilia apter <emilia.apter@auspost.com.au>
2019-01-21 22:08:52 +1100

    refactored tests

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -2,8 +2,8 @@
 class Bottles
   def verse(number)
       <<~VERSE
-        #{remainder(number+1).capitalize} #{utility_factory(number)} of beer on the wall, #{remainder(number+1)} #{utility_factory(number)} of beer.
-        #{action(number)}, #{successor(number)} #{utility_factory(number-1)} of beer on the wall.
+        #{Utility.new(number+1).remainder.capitalize} #{Utility.new(number).container} of beer on the wall, #{Utility.new(number+1).remainder} #{utility_factory(number)} of beer.
+        #{Utility.new(number).action}, #{Utility.new(number).successor} #{utility_factory(number-1)} of beer on the wall.
       VERSE
   end
 
@@ -15,50 +15,54 @@ class Bottles
     verses(99,0)
   end
 
-  def remainder(number)
-    if number == 1
+  def utility_factory(number)
+      utility = Utility.new(number)
+      utility.container
+  end
+end
+
+class Utility
+  def initialize(number)
+    @number = number
+  end
+
+  def container
+    if @number == 1
+      "bottle"
+    else
+      "bottles"
+    end
+  end
+
+  def remainder
+    if @number == 1
       "no more".to_s
     else
-      (number-1).to_s
+      (@number-1).to_s
     end
   end
 
-  def successor(number)
-    if number == 0
+  def successor
+    if @number == 0
       "99"
     else
-      remainder(number)
+      self.remainder
     end
   end
 
-  def action(number)
-    if number == 0
-      "Go to the store and buy some more"
-    else
-      "Take #{pronoun(number)} down and pass it around"
-    end
-  end
-
-  def pronoun(number)
-    if number == 1
+  def pronoun
+    if @number == 1
       "it"
     else
       "one"
     end
   end
 
-  def utility_factory(number)
-      utility = Utility.new
-      utility.container(number)
-  end
-end
-
-class Utility
-    def container(number)
-      if number == 1
-        "bottle"
-      else
-        "bottles"
-      end
+  def action
+    if @number == 0
+      "Go to the store and buy some more"
+    else
+      "Take #{self.pronoun} down and pass it around"
     end
+  end
 end
