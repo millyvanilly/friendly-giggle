f0a19e2c0406aad513c5afcc014c8f2e52785072
emilia apter <emilia.apter@auspost.com.au>
2019-08-05 21:13:05 +1000

    pass arguement

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -10,7 +10,7 @@ class Bottles
         "#{number} #{container(number)} of beer on the wall, " +
         "#{number} #{container(number)} of beer.\n" +
         "Take #{pronoun(number)} down and pass it around, " +
-        "#{quantity} bottles of beer on the wall.\n"
+        "#{quantity(number)} bottles of beer on the wall.\n"
     else
         "#{number} #{container(number)} of beer on the wall, " +
         "#{number} #{container(number)} of beer.\n" +
