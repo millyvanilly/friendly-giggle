5fc8b5f5ec9e651bb38d9858f008a9cb2fc7a3f6
emilia apter <emilia.apter@auspost.com.au>
2019-08-06 20:58:57 +1000

    use else in successor

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -10,7 +10,7 @@ class Bottles
         "#{quantity(number)} #{container(number)} of beer on the wall, " +
         "#{quantity(number)} #{container(number)} of beer.\n" +
         "#{action(number)}, " +
-        "#{quantity(number-1)} #{container(number-1)} of beer on the wall.\n"
+        "#{quantity(successor(number))} #{container(number-1)} of beer on the wall.\n"
     end
   end
 
