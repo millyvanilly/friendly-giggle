73aef02780744cd998e8cbb73371e116447c4af3
emilia apter <emilia.apter@auspost.com.au>
2019-08-06 20:55:22 +1000

    use successor

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -5,7 +5,7 @@ class Bottles
         "#{quantity(number).capitalize} #{container(number)} of beer on the wall, " +
         "#{quantity(number)} #{container(number)} of beer.\n" +
         "#{action(number)}, " +
-        "#{quantity(99)} #{container(number-1)} of beer on the wall.\n"
+        "#{quantity(successor)} #{container(number-1)} of beer on the wall.\n"
     else
         "#{quantity(number)} #{container(number)} of beer on the wall, " +
         "#{quantity(number)} #{container(number)} of beer.\n" +
