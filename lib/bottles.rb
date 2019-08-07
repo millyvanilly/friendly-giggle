a76ed9bcaa8b7bdc2acb58ca309308cddba68833
emilia apter <emilia.apter@auspost.com.au>
2019-08-07 21:02:51 +1000

    Call to_s with quotes

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -2,7 +2,7 @@ class Bottles
   def verse(number)
     bottle_number = BottleNumber.new(number)
     next_bottle_number = BottleNumber.new(bottle_number.successor)
-    "#{bottle_number.quantity} #{bottle_number.container} of beer on the wall, ".capitalize +
+    "#{bottle_number} of beer on the wall, ".capitalize +
     "#{bottle_number.quantity} #{bottle_number.container} of beer.\n" +
     "#{bottle_number.action}, " +
     "#{next_bottle_number.quantity} #{next_bottle_number.container} of beer on the wall.\n"
